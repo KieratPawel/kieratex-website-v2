a29090f4df393f6c2b4285514049580c88cf10dd
Paweł Kierat <kierat.pawel@gmail.com>
2022-02-07 00:16:48 +0100

    add navbar + styles

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,25 @@ import Head from "./Head";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="Page">
       <Background />
-      <Head />
-      <About />
-      <Projects />
-      <Contact />
+      <div className="view_wrapper" id="head">
+        <Head />
+      </div>
+      <div className="view_wrapper" id="about">
+        <About />
+      </div>
+      <div className="view_wrapper" id="projects">
+        <Projects />
+      </div>
+      <div className="view_wrapper" id="contact">
+        <Contact />
+      </div>
+      <Navbar />
     </div>
   );
 }
