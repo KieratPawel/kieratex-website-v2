f6c5c7715673663aa0d281189355458e21b99541
Paweł Kierat <kierat.pawel@gmail.com>
2022-02-07 00:32:42 +0100

    remove navbar from mobiles + update styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,54 +2,58 @@ import React from "react";
 import { Link } from "react-scroll";
 
 function Navbar() {
-  return (
-    <div className="navbar">
-      <Link
-        activeClass="active"
-        to="head"
-        spy={true}
-        smooth={true}
-        hashSpy={true}
-        offset={0}
-        duration={1000}
-      >
-        <span></span>
-      </Link>
-      <Link
-        activeClass="active"
-        to="about"
-        spy={true}
-        smooth={true}
-        hashSpy={true}
-        offset={0}
-        duration={1000}
-      >
-        <span></span>
-      </Link>
-      <Link
-        activeClass="active"
-        to="projects"
-        spy={true}
-        smooth={true}
-        hashSpy={true}
-        offset={0}
-        duration={1000}
-      >
-        <span></span>
-      </Link>
-      <Link
-        activeClass="active"
-        to="contact"
-        spy={true}
-        smooth={true}
-        hashSpy={true}
-        offset={0}
-        duration={1000}
-      >
-        <span></span>
-      </Link>
-    </div>
-  );
+  if (window.innerWidth > 756) {
+    return (
+      <div className="navbar">
+        <Link
+          activeClass="active"
+          to="head"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={0}
+          duration={1000}
+        >
+          <span></span>
+        </Link>
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={0}
+          duration={1000}
+        >
+          <span></span>
+        </Link>
+        <Link
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={0}
+          duration={1000}
+        >
+          <span></span>
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={0}
+          duration={1000}
+        >
+          <span></span>
+        </Link>
+      </div>
+    );
+  } else {
+    return "";
+  }
 }
 
 export default Navbar;
